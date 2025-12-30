ad3bf9abcc4cc4d6ad4fdba07c0db74072517ed5
Kattarin <114744189+Kattarinn@users.noreply.github.com>
2025-12-30 11:52:13 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OEECalculator } from "@/components/oee-calculator"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <OEECalculator />
-    </main>
-  )
-}
