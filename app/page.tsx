27bdb26c4c4f5190d249d4ba5659f1bdcb4eece7
Kattarin <114744189+Kattarinn@users.noreply.github.com>
2025-12-30 11:50:10 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { OEECalculator } from "@/components/oee-calculator"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <OEECalculator />
+    </main>
+  )
+}
